8b2422d7e6596c3ea5635ab424cd8886a4194111
Dmitry Ponyatov <dponyatov@gmail.com>
2013-03-01 19:00:20 +0400

    + manual sources to last commit * keil project fixes

--- src/blink/blink.c
+++ src/blink/blink.c
@@ -1,6 +1,12 @@
-#include <stm32f4xx.h>
+/*
+ * ARMatura book sample
+ * https://github.com/ponyatov/ARMatura
+*/
+
+#include <stm32f4xx.h> // загрузить CMSIS-библиотеку STM32Fx
 
 int main()
 {
 	return 0;
+	char X[]="Hello World !";
 }
