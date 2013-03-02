54816c2e90ea490eaa9f7ae2c5175af387274523
Dmitry Ponyatov <dponyatov@gmail.com>
2013-03-02 22:47:20 +0400

    * some rework on manual tree * keil prj + adapters

---
+++ manual/firststep/blink/blink.c
@@ -0,0 +1,12 @@
+/*
+ * ARMatura book sample
+ * https://github.com/ponyatov/ARMatura
+*/
+
+#include <stm32f4xx.h> // загрузить CMSIS-библиотеку STM32Fx
+
+int main()
+{
+	return 0;
+// проверка отображения строк в LaTeX:	char X[]="Hello World !";
+}
